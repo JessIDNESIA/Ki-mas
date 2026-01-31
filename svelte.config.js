eefbc3fc738cc494cbf13e720e681792d6ec97e8
Si jose <joseidnn1@gmail.com>
2026-01-31 10:20:31 +0700

    chore: set adapter-vercel with nodejs20

--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
-    kit: {
-        // Ubah bagian adapter menjadi seperti ini:
-        adapter: adapter({
-            runtime: 'nodejs20.x' // <--- TAMBAHKAN BARRIS INI
-        })
-    }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs20.x'
+    })
+  }
 };
 
 export default config;
-
