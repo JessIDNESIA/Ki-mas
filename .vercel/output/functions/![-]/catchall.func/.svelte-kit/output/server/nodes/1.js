e16e4e91f185ce5d69c51649be236ed9c10e2733
Si jose <joseidnn1@gmail.com>
2026-02-18 07:47:46 +0700

    chore: ignore .vercel, stop tracking build output

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CLWhxET_.js","_app/immutable/chunks/BukE7bu8.js","_app/immutable/chunks/DPVsqABC.js","_app/immutable/chunks/BtvI-ONE.js"];
-export const stylesheets = [];
-export const fonts = [];
